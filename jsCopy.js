4f1476f69088e0146ef99aa920385c736a5a09aa
Siew Kam Onn <kosiew@gmail.com>
2024-02-05 13:03:15 +0800

    navigator.clipboard to copy

--- jsCopy.js
+++ jsCopy.js
@@ -13,38 +13,14 @@ javascript: (function () {
   }
 
   function copyToClipboard(text) {
-    const textArea = document.createElement("textarea");
-
-    textArea.style.position = "fixed";
-    textArea.style.top = 0;
-    textArea.style.left = 0;
-
-    textArea.style.width = "2em";
-    textArea.style.height = "2em";
-
-    textArea.style.padding = 0;
-
-    textArea.style.border = "none";
-    textArea.style.outline = "none";
-    textArea.style.boxShadow = "none";
-
-    textArea.style.background = "transparent";
-
-    textArea.value = text;
-
-    document.body.appendChild(textArea);
-    textArea.focus();
-    textArea.select();
-
-    try {
-      const successful = document.execCommand("copy");
-      const msg = successful ? "successful" : "unsuccessful";
-      dlog("Copying text command was " + msg);
-    } catch (err) {
-      dlog("Oops, unable to copy");
-    }
-
-    document.body.removeChild(textArea);
+    navigator.clipboard
+      .writeText(text)
+      .then(() => {
+        console.log("Text copied to clipboard");
+      })
+      .catch((error) => {
+        console.error("Error copying text: ", error);
+      });
   }
 
   function copyItemsToClipboard(items) {
