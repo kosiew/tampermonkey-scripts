c8d8a84ea2cd27d72bee8a72e849ed0dbe02520f
Siew Kam Onn <kosiew@gmail.com>
2024-02-05 13:00:47 +0800

    Add showSearchFilterPills

--- testD1.js
+++ testD1.js
@@ -7,6 +7,7 @@ javascript: (function () {
     { name: "enableComments", value: true },
     { name: "playwrightShowSharedJournals", value: true },
     { name: "showSearchButton", value: true },
+    { name: "showSearchFilterPills", value: true },
     { name: "enableE2EEKeyGeneration", value: true },
     { name: "showNewTimelineRows", value: true },
     { name: "enablePasskeys", value: true },
