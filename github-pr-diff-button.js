88c6a7929f3c3bde594741285e904309a731ed82
Siew Kam Onn <kosiew@gmail.com>
2025-04-15 09:08:16 +0800

    Bump version to 1.1 and enhance diff button functionality with error handling for URL extraction
    amend this to handle PR related links like
    https://github.com/apache/datafusion/pull/15626/files
    https://github.com/apache/datafusion/pull/15626#pullrequestreview-2748097750

--- github-pr-diff-button.js
+++ github-pr-diff-button.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         GitHub PR Diff Button
 // @namespace    http://tampermonkey.net/
-// @version      1.0
+// @version      1.1
 // @description  Adds a button to view PR diff in new tab
 // @author       Siew Kam Onn
 // @match        https://github.com/*/pull/*
@@ -12,6 +12,10 @@
 (function () {
   "use strict";
 
+  /**
+   * Adds a "View Diff" button next to the Code button in GitHub PR pages
+   * @returns {void}
+   */
   function addDiffButton() {
     // Find the Code button
     const codeButton = document.querySelector(
@@ -31,8 +35,19 @@
     diffButton.className = "btn btn-sm diff-view-button ml-2";
     diffButton.innerHTML = "View Diff";
     diffButton.onclick = () => {
-      const currentUrl = window.location.href;
-      GM.openInTab(`${currentUrl}.diff`, true);
+      try {
+        const currentUrl = window.location.href;
+        // Extract the base PR URL (remove hash fragments and extra paths after the PR number)
+        const match = currentUrl
+          .split("#")[0]
+          .match(/https:\/\/github\.com\/[^/]+\/[^/]+\/pull\/\d+/);
+        const basePrUrl = match ? match[0] : currentUrl;
+        GM.openInTab(`${basePrUrl}.diff`, true);
+      } catch (error) {
+        console.error("GitHub PR Diff Button: Error opening diff view", error);
+        // Fall back to current URL if parsing fails
+        GM.openInTab(`${window.location.href}.diff`, true);
+      }
     };
 
     // Insert after the details element
@@ -52,4 +67,7 @@
 
   // Run on page load
   window.addEventListener("load", addDiffButton);
+
+  // Run immediately in case the page is already loaded
+  addDiffButton();
 })();
